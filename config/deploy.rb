8f95d7a36dad1f4c57246e7ba8875d37ba01ff3c
lichtshinoza <licht7xinoza@gmail.com>
2017-09-30 17:54:21 +0900

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.9.1'
 
 set :application, 'Airbnb'
 set :repo_url,  'git@github.com:lichtshinoza/Airbnb.git'
+
+set :deploy_to, '/var/www/Airbnb/current'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -15,6 +17,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.114.14.150', user: 'ec2-user', roles: %w{app db web}
+set :ssh_options, keys: ['/Users/shiinozamb/.ssh/skeypair.pem']
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
