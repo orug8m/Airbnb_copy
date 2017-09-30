ee3d726da8c1a308463db87014fc2174388047b7
lichtshinoza <licht7xinoza@gmail.com>
2017-09-30 18:25:20 +0900

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/shiinozamb/.ssh/skeypair.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :unicorn_pid, -> { "/var/www/Airbnb/current/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "/var/www/Airbnb/current/config/unicorn.rb" }
 set :keep_releases, 5
 set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 
