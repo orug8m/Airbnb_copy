bb4420021c679453f3c51d74318e458b1a33eadb
lichtshinoza <licht7xinoza@gmail.com>
2017-09-30 18:38:37 +0900

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.9.1'
 set :application, 'Airbnb'
 set :repo_url,  'git@github.com:lichtshinoza/Airbnb.git'
 
-set :deploy_to, '/var/www/Airbnb/current'
+# set :deploy_to, '/var/www/Airbnb/current/'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -14,8 +14,8 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/shiinozamb/.ssh/skeypair.pem']
 
-set :unicorn_pid, -> { "/var/www/Airbnb/current/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "/var/www/Airbnb/current/config/unicorn.rb" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 
