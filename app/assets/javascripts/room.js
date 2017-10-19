a44e2c720138a40b62185b7c197bd22e3675cc2e
lichtshinoza <licht7xinoza@gmail.com>
2017-10-19 23:19:43 +0900

    1.remove development.log from Github, 2.temp text converted to #{@var.method}, 3.some 'id' modified to 'class'

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,12 +1,12 @@
 $(function(){
   $(document).on('click','.input_guests', function(e){
     e.preventDefault();
-      $('#select_area_contents').toggle()
+      $('.select_area_contents').toggle()
 
   });
   $(document).on('click touchend', function(event) {
-    if (!$(event.target).closest('#selectarea, .input_guests').length) {
-      $('#select_area_contents').hide();
+    if (!$(event.target).closest('.selectarea, .input_guests').length) {
+      $('.select_area_contents').hide();
     }
   });
 });
