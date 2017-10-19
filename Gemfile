0a5bbf13a5cd8298b1d6397d8874288e7932ac6b
lichtshinoza <licht7xinoza@gmail.com>
2017-10-19 11:30:17 +0900

    datatimepick gem installing(processing...)

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'rails-i18n'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'fog'
+gem 'momentjs-rails', '>= 2.9.0'
+gem 'bootstrap3-datetimepicker-rails', '~> 4.17.47'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -9,4 +9,16 @@ $(function(){
       $('#select_area_contents').hide();
     }
   });
+  //calender function start here
+  $('#datetimepicker6').datetimepicker();
+  $('#datetimepicker7').datetimepicker({
+      useCurrent: false
+  });
+  $("#datetimepicker6").on("dp.change", function (e) {
+    $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+  });
+  $("#datetimepicker7").on("dp.change", function (e) {
+    $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+  });
+  //calender function fin
 });
